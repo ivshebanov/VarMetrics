bf3d142ae5b1881058e8fc2df8ab3ab230ef0728
ivshebanov <ivshebanov@gmail.com>
2020-10-02 19:07:08 +0000

    VAR-3 Добавил скрипты для h2

---
+++ src/main/resources/sql/test_data.sql
@@ -0,0 +1,24 @@
+INSERT INTO dollar (id, date, course)
+VALUES (1, '2009-06-04 12:54:23', '78,234');
+
+INSERT INTO dollar (id, date, course)
+VALUES (2, '2009-06-03 12:54:23', '77,234');
+
+INSERT INTO dollar (id, date, course)
+VALUES (3, '2009-06-03 12:54:23', '76,234');
+
+INSERT INTO dollar (id, date, course)
+VALUES (4, '2009-06-09 12:54:23', '75,234');
+
+
+INSERT INTO euro (id, date, course)
+VALUES (1, '2009-06-08 12:54:23', '78,234');
+
+INSERT INTO euro (id, date, course)
+VALUES (2, '2009-06-07 12:54:23', '77,234');
+
+INSERT INTO euro (id, date, course)
+VALUES (3, '2009-06-06 12:54:23', '76,234');
+
+INSERT INTO euro (id, date, course)
+VALUES (4, '2009-06-05 12:54:23', '75,234');
