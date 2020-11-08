3348250ff5b589fa0b63b0c8a70e451fcc5bdbd7
Ilya Shebanov <Shebanov.I.V@sberbank.ru>
2020-11-08 23:38:26 +0300

    VAR-26 Добавил thymeleaf

--- src/main/java/com/varmetrics/endpoint/start/StartEndpoint.java
+++ src/main/java/com/varmetrics/endpoint/start/StartEndpoint.java
@@ -2,24 +2,32 @@ package com.varmetrics.endpoint.start;
 
 import com.varmetrics.service.WriteUsdAndEur;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/start")
+@RequestMapping
 public class StartEndpoint {
 
     private final WriteUsdAndEur writeUsdAndEur;
+
+    @Value("${spring.profiles.active}")
+    private String profile;
 
     @Autowired
     public StartEndpoint(WriteUsdAndEur writeUsdAndEur) {
         this.writeUsdAndEur = writeUsdAndEur;
     }
 
-    @GetMapping("/run")
-    public void setUsd() {
-        writeUsdAndEur.runWrite();
+    @GetMapping
+    public String main(Model model) {
+
+        model.addAttribute("isDevMode", "1DEV".equals(profile));
+
+        return "index";
     }
 
     @GetMapping("/interrupt")
