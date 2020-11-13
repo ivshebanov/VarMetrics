5c53004feef5f0bcdadbec3ae470f70f70aa0de8
Ilya Shebanov <Shebanov.I.V@sberbank.ru>
2020-11-13 22:53:40 +0300

    VAR-26 Окружение для фронта

--- src/main/java/com/varmetrics/endpoint/start/StartEndpoint.java
+++ src/main/java/com/varmetrics/endpoint/start/StartEndpoint.java
@@ -3,18 +3,18 @@ package com.varmetrics.endpoint.start;
 import com.varmetrics.service.WriteUsdAndEur;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping
 public class StartEndpoint {
 
     private final WriteUsdAndEur writeUsdAndEur;
 
-    @Value("${spring.profiles.active}")
+    @Value("${spring.profiles.active:PROD}")
     private String profile;
 
     @Autowired
@@ -25,7 +25,7 @@ public class StartEndpoint {
     @GetMapping
     public String main(Model model) {
 
-        model.addAttribute("isDevMode", "1DEV".equals(profile));
+        model.addAttribute("isDevMode", "DEV".equals(profile));
 
         return "index";
     }
