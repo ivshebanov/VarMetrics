ac2c90da0df87242c9e9717727d6af98524a2eca
Ilya Shebanov <Shebanov.I.V@sberbank.ru>
2020-11-23 15:01:17 +0300

    VAR-29 Поменял отображение даты на "yyyy-MM-dd HH:mm:ss.SSS Z"

--- src/main/java/com/varmetrics/dao/dateFormat/ZonedDateTimeDeserializer.java
+++ src/main/java/com/varmetrics/dao/dateFormat/ZonedDateTimeDeserializer.java
@@ -1,4 +1,4 @@
-package com.varmetrics.dao;
+package com.varmetrics.dao.dateFormat;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
@@ -11,8 +11,10 @@ import java.util.Locale;
 
 public class ZonedDateTimeDeserializer extends JsonDeserializer<ZonedDateTime> {
 
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS Z");
+
     @Override
     public ZonedDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
-        return ZonedDateTime.parse(jsonParser.getText(), DateTimeFormatter.ISO_ZONED_DATE_TIME);
+        return ZonedDateTime.parse(jsonParser.getText(), DATE_TIME_FORMATTER);
     }
 }
