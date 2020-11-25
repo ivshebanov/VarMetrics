93b07bf7fe55f2f5a5064fff13e371196ae2da20
Ilya Shebanov <Shebanov.I.V@sberbank.ru>
2020-11-25 20:08:17 +0300

    VAR-30 Редактирование таблицы

--- src/main/resources/js/components/tables/CourseTable.vue
+++ src/main/resources/js/components/tables/CourseTable.vue
@@ -1,42 +1,5 @@
 <template>
-  <v-data-table height="80vh"
-                :headers="headers"
-                :items="list"
-                :items-per-page="5"
-                class="elevation-1"
-                :footer-props="{
-      showFirstLastPage: true,
-      firstIcon: 'mdi-arrow-collapse-left',
-      lastIcon: 'mdi-arrow-collapse-right',
-      prevIcon: 'mdi-minus',
-      nextIcon: 'mdi-plus'
-    }"
-  ></v-data-table>
-  <!--  <v-simple-table height="400"-->
-  <!--                  v-model="selected"-->
-  <!--                  :headers="headers"-->
-  <!--                  fixed-header-->
-  <!--                  :items="desserts"-->
-  <!--                  item-key="name">-->
-  <!--    <template v-slot:default>-->
-  <!--      <thead>-->
-  <!--      <tr>-->
-  <!--        <th class="text-left">-->
-  <!--          Дата-->
-  <!--        </th>-->
-  <!--        <th class="text-left">-->
-  <!--          Курс-->
-  <!--        </th>-->
-  <!--      </tr>-->
-  <!--      </thead>-->
-  <!--      <tbody>-->
-  <!--      <tr v-for="el in list" :key="el.id">-->
-  <!--        <td>{{ el.date }}</td>-->
-  <!--        <td>{{ el.course }}</td>-->
-  <!--      </tr>-->
-  <!--      </tbody>-->
-  <!--    </template>-->
-  <!--  </v-simple-table>-->
+  <v-data-table :headers="headers" :items="list" :items-per-page="5" class="elevation-1"/>
 </template>
 
 <script>
