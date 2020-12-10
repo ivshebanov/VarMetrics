f40c74a68a05daefaa97b85bce04c6472ad78538
Ilya Shebanov <Shebanov.I.V@sberbank.ru>
2020-12-10 16:10:05 +0300

    VAR-31 Отредактировал размеры колонок

--- src/main/resources/migration/sql/0001_ddl_VarMetrics_RELEASE_1.sql
+++ src/main/resources/migration/sql/0001_ddl_VarMetrics_RELEASE_1.sql
@@ -27,15 +27,15 @@ CREATE TABLE VAR_METRICS.eur
 -- changeset ivshebanov:ddl_initial_step_varmetrics#0005
 CREATE TABLE VAR_METRICS.vacancy
 (
-    id          BIGINT                 NOT NULL,
-    title       CHARACTER VARYING(100) NOT NULL,
-    companyName CHARACTER VARYING(50)  NOT NULL,
-    companyLogo CHARACTER VARYING(100) NOT NULL,
-    salary      CHARACTER VARYING(50),
-    location    CHARACTER VARYING(50),
-    siteName    CHARACTER VARYING(50)  NOT NULL,
-    url         CHARACTER VARYING(100) NOT NULL,
-    dateVacancy CHARACTER VARYING(50),
-    date        TIMESTAMP              NOT NULL,
+    id           BIGINT                 NOT NULL,
+    title        CHARACTER VARYING(200) NOT NULL,
+    company_name CHARACTER VARYING(200)  NOT NULL,
+    company_logo CHARACTER VARYING(200),
+    salary       CHARACTER VARYING(200),
+    location     CHARACTER VARYING(200),
+    site_name    CHARACTER VARYING(200)  NOT NULL,
+    url          CHARACTER VARYING(200) NOT NULL,
+    date_vacancy CHARACTER VARYING(200),
+    date         TIMESTAMP              NOT NULL,
     PRIMARY KEY (id)
 );
