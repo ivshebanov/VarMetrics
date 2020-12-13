a8e8a231d288d34c947c942857e07b720ea3fa90
Ilya Shebanov <Shebanov.I.V@sberbank.ru>
2020-12-13 17:28:37 +0300

    VAR-33 Вкладка вакансий

--- src/main/resources/js/main.js
+++ src/main/resources/js/main.js
@@ -12,30 +12,3 @@ new Vue({
     render: a => a(App),
     vuetify: new Vuetify({}),
 })
-
-// var usdUrl = Vue.resource("/usd");
-// var eurUrl = Vue.resource("/eur");
-
-
-// Vue.component('message-row', {
-//     props: ['val'],
-//     template: '<div>{{ val.date }} ---> {{ val.course }}</div>'
-// })
-//
-// Vue.component('message-list', {
-//     props: ['usdList', 'eurList'],
-//     template:
-//
-//     created: function() {
-//         usdUrl.get().then(result =>
-//             result.json().then(data =>
-//                 data.forEach(usd => this.usdList.push(usd)),
-//             )
-//         )
-//         eurUrl.get().then(result =>
-//             result.json().then(data =>
-//                 data.forEach(eur => this.eurList.push(eur)),
-//             )
-//         )
-//     },
-// })
---
+++ src/main/resources/js/pages/VacancyTab.vue
@@ -0,0 +1,134 @@
+<template>
+  <v-main>
+    <v-container>
+      <v-btn
+          class="ma-2"
+          :loading="loading"
+          :disabled="loading"
+          color="info"
+          @click="loader = 'loading'"
+      >
+        Загрузить вакансии
+        <template v-slot:loader>
+        <span class="custom-loader">
+          <v-icon light>cached</v-icon>
+        </span>
+        </template>
+      </v-btn>
+
+
+      <v-card>
+        <v-card-title>
+          Вакансии
+          <v-spacer/>
+          <v-text-field v-model="search"
+                        append-icon="mdi-magnify"
+                        label="Поиск"
+                        single-line
+                        hide-details/>
+        </v-card-title>
+        <v-data-table height="70vh"
+                      :headers="headers"
+                      :items="vacancyList"
+                      :search="search"
+                      :loading="loading"
+                      loading-text="Загрузка... Пожалуйста ждите">
+          <template v-slot:item.companyLogo="{ item }">
+            <v-img :src="item.companyLogo"/>
+          </template>
+          <template v-slot:item.title="{ item }">
+            <a :href="item.url" target="_blank">{{ item.title }}</a>
+          </template>
+        </v-data-table>
+      </v-card>
+    </v-container>
+  </v-main>
+</template>
+
+<script>
+export default {
+  name: "Vacancy",
+  data() {
+    return {
+      headers: [
+        {text: '', value: 'companyLogo', width: "10%", align: 'start', sortable: false},
+        {text: 'Вакансия', value: 'title', width: "15%"},
+        {text: 'Компания', value: 'companyName', width: "30%"},
+        {text: 'З/П', value: 'salary', width: "20%"},
+        {text: 'Адрес', value: 'location', width: "15%"},
+        {text: 'Дата размещения', value: 'dateVacancy', width: "10%"},
+      ],
+      search: '',
+      loader: null,
+      loading: false,
+      vacancyList: []
+    }
+  },
+  watch: {
+    loader() {
+      this.l = this.loader
+      this[this.l] = !this[this.l]
+
+      this.$resource("/vacancies/scan").get().then(result =>
+          result.json().then(data => {
+                data.forEach(vacancy => this.vacancyList.push(vacancy))
+                this[this.l] = false
+                this.loader = null
+              }
+          )
+      )
+
+    },
+  },
+  created: function () {
+    this.$resource("/vacancies").get().then(result =>
+        result.json().then(data =>
+            data.forEach(usd => this.vacancyList.push(usd)),
+        )
+    )
+  }
+}
+</script>
+
+<style scoped>
+.custom-loader {
+  animation: loader 1s infinite;
+  display: flex;
+}
+
+@-moz-keyframes loader {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+@-webkit-keyframes loader {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+@-o-keyframes loader {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes loader {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
